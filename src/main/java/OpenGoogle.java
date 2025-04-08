98ec45c7c00b0bad9a084deffa83bf2b90dfdebe
Charutha-Herath <charuh2023@gmail.com>
2025-04-09 02:02:33 +0530

    open google chrome home

---
+++ src/main/java/OpenGoogle.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\ACER\\Downloads\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
