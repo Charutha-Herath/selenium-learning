bd1fa9f213ee27fa81decf84b8d734b0cc070dd7
Charutha-Herath <charuh2023@gmail.com>
2025-04-09 12:15:55 +0530

    done stage 02 of driver downloads

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,10 +1,22 @@
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenGoogle {
+    //Open google
+    //Go to the Google home page
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\ACER\\Downloads\\driver\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver","C:\\Users\\ACER\\Downloads\\driver\\chromedriver.exe");   // only use for Stage 01 implementation
+        //WebDriver driver = new ChromeDriver();
+        //driver.get("https://www.google.com/");
+
+        //============================================================================
+
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+
+
+
     }
 }
