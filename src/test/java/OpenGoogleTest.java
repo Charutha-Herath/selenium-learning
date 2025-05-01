f63aded101e75176924289a82ce9a2b53e1b2f35
Charutha-Herath <charuh2023@gmail.com>
2025-05-01 23:55:43 +0530

    done chromeOptions uses

--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -2,12 +2,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 @Test
 public class OpenGoogleTest {
 
     public void googleTest(){
+
+        //Browser version handling and others using ChromeOptions
+
+        /*ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("122");
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--headless");
+        WebDriver  driver = new ChromeDriver(chromeOptions);*/
+
+
+
         WebDriver driver = new ChromeDriver();  // All ready run 'WebDriverManager.chromedriver().setup();' code in this line since selenium 4 version
         driver.get("https://www.google.com/");
         driver.findElement(By.name("q")).sendKeys("Colombo"+ Keys.ENTER);
